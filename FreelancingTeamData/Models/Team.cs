53448b79ba819459a38efa3c20aca6074d66e8c4
Mostafa M. Gad <mostafagad464@gmail.com>
2022-07-13 14:16:41 +0200

    update team model

--- FreelancingTeamData/Models/Team.cs
+++ FreelancingTeamData/Models/Team.cs
@@ -41,7 +41,6 @@ namespace FreelancingTeamData.Models
         public int? WalletId { get; set; }
         public string? Name { get; set; }
 
-        public string? Name { get; set; }
 
         [ForeignKey("LeaderId")]
         [InverseProperty("Teams")]
